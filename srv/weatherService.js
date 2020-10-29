f752d31c1b9a090ea6e92c5d15a6e47d2755d0cb
i536490 <quentin.siewe.tchuente@sap.com>
2020-10-29 20:10:59 +0000

    Statusquo

--- srv/weatherService.js
+++ srv/weatherService.js
@@ -31,56 +31,36 @@ async function _fetchCurrentWeatherData(req) {
         pwd: "OKH(Ba(#aWYH9eA<",
     };
 
-    //get relevant cities
     var cities = await selectCities(conn_parms, conn);
-
-    /*conn.exec("INSERT INTO WEATHERDATA.WEATHERBYPOSTALCODE VALUES('2d76de7e-af14-4371-abcf-65ae0b0dcce2', 502, 11, '2020-10-29T14:55:16.000Z')", function (err, affectedRows) {
-              if (err) throw err;
-              console.log('Number of affected rows:', affectedRows);
-          });
-
-    //get weather data for city and insert in the DWC
-    for (var i = 0; i < cities.length; i++) {
-        var city = cities[i];
-        const weatherData = await getWeather(conn_parms, conn, city);
-        //var currentData = conn.exec('SELECT * FROM "WEATHERDATA"."WEATHERBYPOSTALCODE" ORDER BY LASTUPDATE');
-        //console.log(currentData);
-        console.log(city);
-        console.log(weatherData);
-        //conn.connect();
-        //var test = "INSERT INTO WEATHERBYPOSTALCODE VALUES('" + uuidv4() + "', " + weatherData.weatherConditionID + ", " + weatherData.temperature + ", '" + weatherData.lastUpdate + "')"
-        /*if (currentData.length === 0) {
-            conn.exec("INSERT INTO WEATHERBYPOSTALCODE VALUES('" + uuidv4() + " )", function (err, affectedRows) {
-                if (err) throw err;
-                console.log('Number of affected rows:', affectedRows);
-            });
-        }
-        //console.log(test);
-
-    }*/
     conn.disconnect();
 }
 
 async function selectCities(conn_parms, conn) {
     conn.connect(conn_parms);
-    var result = conn.exec('SELECT "Country","City","Postalcode" FROM "WEATHERDATA"."Relevant_Cities_for_API"');
-    var currentData = conn.exec('SELECT * FROM "WEATHERBYPOSTALCODE" ORDER BY LASTUPDATE');
+    var result = conn.exec('SELECT * FROM "WEATHERDATA"."Relevant_Cities_for_API"');
+    
+
     conn.exec("SET 'TIMESTAMP_FORMAT' = 'YYYY/MM/DD HH:MI:SS'")
-    const weatherData = await getWeather(conn_parms, conn, result[0]);
-    console.log(weatherData);
-    conn.exec(`INSERT INTO WEATHERBYPOSTALCODE VALUES('${uuidv4().toString()}', ${weatherData.postalCode}, ${weatherData.weatherConditionID}, ${weatherData.temperature}, '${weatherData.lastUpdate}')`, function (err, affectedRows) {
-        if (err) console.error(err);
-        console.log('Number of affected rows:', affectedRows);
-    });
-    console.log(currentData);
+
+    for (var i = 0; i < result.length; i++) {
+        var id = uuidv4().toString();
+        const weatherData = await getWeather(conn_parms, conn, result[i]);
+        var currentData = conn.exec('SELECT * FROM "WEATHERBYPOSTALCODE" ORDER BY LASTUPDATE');
+        console.log(currentData);
+        //if (currentData.length === 0) {
+            conn.exec(`INSERT INTO WEATHERBYPOSTALCODE VALUES('${id}', ${weatherData.postalCode}, ${weatherData.weatherConditionID}, ${weatherData.temperature}, '${weatherData.lastUpdate}')`, function (err, affectedRows) {
+                if (err) console.error(err);
+                console.log('Number of affected rows:', affectedRows);
+            });
+        //}
+    }
     conn.disconnect();
     return result;
 }
 
 async function getWeather(conn_parms, conn, city) {
     var key = config.keys.crawler[0];
-
-    //conn.connect(conn_parms);
+    console.log(city);
     const url = config.url.replace(/%s/g, encodeURI(`${city.Postalcode}`)).replace(/%c/g, encodeURI(`${city.Country}`))
         .replace(/%k/g, encodeURI(key));
 
@@ -104,13 +84,10 @@ async function getWeather(conn_parms, conn, city) {
         console.error("Table already exists");
     }
     let postalCode = city.Postalcode;
-    console.log(moment(new Date(json.dt * 1000)).format("DD/MM/YYYY hh:mm:ss"))
     return {
         postalCode: postalCode.toString(),
         weatherConditionID: json.weather[0].id,
         temperature: json.main.temp,
         lastUpdate: moment(new Date(json.dt * 1000)).format("YYYY/MM/DD hh:mm:ss"),
     };
-
-    //conn.disconnect();
 }
