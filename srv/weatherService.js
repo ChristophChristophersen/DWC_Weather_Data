9598586ca0e33dd3cc63e8d245f3cf1cf334d3eb
Christophersen <christoph.christophersen@sap.com>
2020-11-05 16:54:33 +0100

    Clean + Upsert + Weatherconditions

--- srv/weatherService.js
+++ srv/weatherService.js
@@ -4,7 +4,8 @@ module.exports = cds.service.impl(srv => {
 
 const fetch = require('node-fetch');
 const { v4: uuidv4 } = require('uuid');
-const moment = require('moment')
+const moment = require('moment');
+const { array } = require('@sap/cds');
 
 const config = {
     "url": "http://api.openweathermap.org/data/2.5/weather?q=%s,%c&appid=%k&units=metric",
@@ -30,34 +31,34 @@ async function _fetchCurrentWeatherData(req) {
         uid: "WEATHERDATA#MASTER",
         pwd: "OKH(Ba(#aWYH9eA<",
     };
+    /*
+    conn.connect(conn_parms);
+    try {
+        conn.exec('CREATE TABLE WEATHERBYPOSTALCODE(ID NVARCHAR(50), POSTALCODE NVARCHAR(10), WEATHERCONDITIONSID Integer, TEMPERATURE Double, SOURCEUPDATE Timestamp, PRIMARY KEY (ID))');
+    } catch (error) {
+        console.log("Table WEATHERBYPOSTALCODE already exists");
+    }
 
-    var cities = await selectCities(conn_parms, conn);
+    try {
+        conn.exec('CREATE TABLE WEATHERCONDITIONS(ID INTEGER, MAIN NVARCHAR(50), DESCRIPTION NVARCHAR(50), PRIMARY KEY (ID));');
+    } catch (error) {
+        console.log("Table WEATHERCONDITIONS already exists");
+    }
     conn.disconnect();
+    */
+    var cities = selectCities(conn_parms, conn);
+    insertWeather(conn_parms, conn, await cities);
 }
 
 async function selectCities(conn_parms, conn) {
     conn.connect(conn_parms);
-    var result = conn.exec('SELECT * FROM "WEATHERDATA"."Relevant_Cities_for_API"');
-    
-
-    conn.exec("SET 'TIMESTAMP_FORMAT' = 'YYYY/MM/DD HH:MI:SS'")
-
-    for (var i = 0; i < result.length; i++) {
-        var id = uuidv4().toString();
-        const weatherData = await getWeather(conn_parms, conn, result[i]);
-        var currentData = conn.exec('SELECT * FROM "WEATHERBYPOSTALCODE" ORDER BY LASTUPDATE');
-        console.log(currentData);
-        //if (currentData.length === 0) {
-            conn.exec(`INSERT INTO WEATHERBYPOSTALCODE VALUES('${id}', ${weatherData.postalCode}, ${weatherData.weatherConditionID}, ${weatherData.temperature}, '${weatherData.lastUpdate}')`, function (err, affectedRows) {
-                if (err) console.error(err);
-                console.log('Number of affected rows:', affectedRows);
-            });
-        //}
-    }
+    var result = await conn.exec('SELECT * FROM "WEATHERDATA"."Relevant_Cities_for_API"');
+    console.log(result.length + " relevant cities in source table");
     conn.disconnect();
     return result;
 }
 
+// get weather for a city (ZIP code + country code) via OpenWeatherMap API
 async function getWeather(conn_parms, conn, city) {
     var key = config.keys.crawler[0];
     console.log(city);
@@ -65,29 +66,42 @@ async function getWeather(conn_parms, conn, city) {
         .replace(/%k/g, encodeURI(key));
 
     const response = await fetch(url);
-    const json = await response.json();
-    try {
-        conn.exec('CREATE TABLE WEATHERBYPOSTALCODE(ID NVARCHAR(50), POSTALCODE NVARCHAR(10), WEATHERCONDITIONSID Integer, TEMPERATURE Double, LASTUPDATE Timestamp, PRIMARY KEY (ID))');
-    } catch (error) {
-        console.log("Table already exists");
-    }
+    const openWeatherJson = await response.json();
 
-    try {
-        conn.exec('CREATE TABLE WEATHERCONDITIONS(ID INTEGER, MAIN NVARCHAR, DESCRIPTION NVARCHAR, PRIMARY KEY (ID));');
-    } catch (error) {
-        console.log("Table already exists");
-    }
-
-    try {
-        conn.exec('CREATE TABLE WEATHERCONDITIONS(ID INTEGER, MAIN NVARCHAR, DESCRIPTION NVARCHAR, PRIMARY KEY (ID));');
-    } catch (error) {
-        console.error("Table already exists");
-    }
     let postalCode = city.Postalcode;
     return {
         postalCode: postalCode.toString(),
-        weatherConditionID: json.weather[0].id,
-        temperature: json.main.temp,
-        lastUpdate: moment(new Date(json.dt * 1000)).format("YYYY/MM/DD hh:mm:ss"),
+        weatherConditionID: openWeatherJson.weather[0].id,
+        weatherConditionMain: openWeatherJson.weather[0].main,
+        weatherConditionDescription: openWeatherJson.weather[0].description,
+        temperature: openWeatherJson.main.temp,
+        sourceUpdate: moment(new Date(openWeatherJson.dt * 1000)).format("YYYY/MM/DD hh:mm:ss"),
+
     };
+}
+
+
+async function insertWeather(conn_parms, conn, weatherResult) {
+    conn.connect(conn_parms);
+    conn.exec("SET 'TIMESTAMP_FORMAT' = 'YYYY/MM/DD HH:MI:SS'")
+    var execArray = [];
+    for (var i = 0; i < weatherResult.length; i++) {
+        var id = uuidv4().toString();
+        const weatherData = await getWeather(conn_parms, conn, weatherResult[i]);
+        var sqlWeather = `UPSERT WEATHERBYPOSTALCODE VALUES('${id}', ${weatherData.postalCode}, ${weatherData.weatherConditionID}, ${weatherData.temperature}, '${weatherData.sourceUpdate}') WHERE POSTALCODE = ${weatherData.postalCode} AND SOURCEUPDATE = '${weatherData.sourceUpdate}'`;
+        var sqlConditions = `UPSERT WEATHERCONDITIONS VALUES(${weatherData.weatherConditionID}, '${weatherData.weatherConditionMain}', '${weatherData.weatherConditionDescription}') WITH PRIMARY KEY`;
+        console.log("i = " + i)
+        execArray.push(sqlExec(sqlWeather, conn));
+        execArray.push(sqlExec(sqlConditions, conn));
+    }
+    await Promise.all(execArray);
+    conn.disconnect();
+}
+
+async function sqlExec(sqlStatement, conn) {
+    return new Promise ((resolve, reject) => {conn.exec(sqlStatement, function (err, affectedRows) {
+        if (err) return reject(err);
+        resolve(affectedRows);
+        });
+    });
 }
